88e92d1efc42a0da2a5c7d6b967aeb32fe6857a5
Jeffrey Lee <jglee1@msn.com>
2019-11-18 23:51:20 -0800

    Feature: Created post

--- server/index.js
+++ server/index.js
@@ -25,7 +25,20 @@ app.get('/api/funkoPops', (req, res) => {
     console.log(data);
     res.status(200).json(data);
   })
-})
+});
+
+app.post('/api/funkoPops', (req, res) => {
+  const { id, item_name, item_number, category, item_collection, release_date, price, collection_value, where_to_buy } = req.query;
+  var queryStr = 'INSERT INTO funkoPops (id, item_name, item_number, category, item_collection, release_date, price, collection_value, where_to_buy) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)';
+  pool.query(queryStr, [id, item_name, item_number, category, item_collection, release_date, price, collection_value, where_to_buy], (err, results) => {
+    if (err) {
+      console.log('Failed to insert: ', err);
+    }
+    res.status(201).json(results);
+  })
+});
+
+app.put('/api/funkoPops',)
 
 // listening
 app.listen(port, () => { console.log(`listening on ${port}`); })
