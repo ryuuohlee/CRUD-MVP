08bade4ed92503e0fe09081e783436c40eefedfe
Jeffrey Lee <jglee1@msn.com>
2019-11-18 22:28:01 -0800

    Feature: completed get connection and webpack

--- server/database/connection.js
+++ server/database/connection.js
@@ -4,15 +4,17 @@ const Pool = require('pg').Pool;
 
 //db connection
 const pool = new Pool({
-  user: process.env.POSTGRES_USER || 'jeffreylee',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  database: process.env.POSTGRES_DATABASE || 'collection',
-  password: process.env.POSTGRES_PASSWORD || '',
-  port: process.env.POSTGRES_PORT || 5432
+  user: 'jeffreylee',
+  host: 'localhost',
+  database: 'collection',
+  password: '',
+  port: 5432
 });
 
 pool.connect()
   .then(() => { console.log('db connected'); })
   .catch(e => { console.log('error', e)});
 
-
+module.exports = {
+  pool
+}
--- server/index.js
+++ server/index.js
@@ -1,17 +1,33 @@
 // requirements
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+// const path = require('path');
+const pool = require('./database/connection.js').pool;
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT;
 
 // middleware
-
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static());
 
 // routes
-
+// get all
+app.get('/api/funkoPops', (req, res) => {
+  pool.query('SELECT * FROM FunkoPops', (err, data) => {
+    if(err){
+      return console.log('error', err);
+    }
+    console.log(data);
+    res.status(200).json(data);
+  })
+})
 
 // listening
 app.listen(port, () => { console.log(`listening on ${port}`); })
 
-module.exports.app = app;
+module.exports = app;
